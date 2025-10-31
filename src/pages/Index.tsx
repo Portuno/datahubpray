6db675227d709e971bcd50c757e56592b677568e
Joaquin Sarni <joaquin.sarni@gmail.com>
2025-10-31 10:40:57 +0100

    ?

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { FilterSidebar } from "@/components/FilterSidebar";
 import { PriceRecommendationCard } from "@/components/PriceRecommendationCard";
